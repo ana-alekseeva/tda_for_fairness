7f88d49da07d66abf1d4efe424ea0d35bad2cbe0
Anastasiia Alekseeva <alekseevaas21@gmail.com>
2024-09-18 23:19:17 +0200

    add vis_scores

--- exp_bert/vis_scores.py
+++ exp_bert/vis_scores.py
@@ -74,10 +74,10 @@ def main():
     
     df_tda_scores_stat = pd.DataFrame(columns=["sample","min","mean","max", "std"])
     df_tda_scores_group_stat = pd.DataFrame(columns=["group","min","mean","max", "std"])
+    df_d3m_scores_stat = pd.DataFrame(columns=["min","mean","max", "std"])
 
     for method in ["IF", "TRAK"]:
         scores = torch.load(f"{args.output_dir}/{args.method}_scores.pt")
-        scores = scores.T
         assert scores.shape[0] < scores.shape[1]
 
         groups = list(test_df["group"].unique())
@@ -110,12 +110,12 @@ def main():
             ax.hist(scores_group, bins=50, alpha=0.7, color='blue', label='Training Samples')
             ax.set_title(f'Distribution of {method} Scores for Group {group}')
             ax.legend()
-            fig.savefig(f'{args.path_to_save}attr_scores/Distribution_of_{method}_scores_for_group_{group}.pdf')
+            fig.savefig(f'{args.path_to_save}attr_scores/Distribution_of_{method}_scores_{group}.pdf')
 
             df_tda_scores_group_stat.loc[method] = [group,scores_group.min(), scores_group.mean(), scores_group.max(), scores_group.std()]
 
 
-        # 3.a. Distribution of group attribution scores for training samples: histograms and a table of means and stds 
+        # 3. Distribution of group attribution scores for training samples: histograms and a table of means and stds 
 
         d3m = D3M(
             model=base_model,
@@ -124,15 +124,31 @@ def main():
             val_dataloader = test_dl,
             group_indices_train=train_group_indices,
             group_indices_val=test_group_indices,
-            scores=scores,
+            scores=scores.T,
             train_set_size=None,
             val_set_size=None)
+        
+        group_losses = d3m.get_group_losses(
+            model=d3m.model,
+            val_dl=d3m.dataloaders["val"],
+            group_indices=d3m.group_indices_val,
+        )
+
+        d3m_scores = d3m.compute_group_alignment_scores(d3m.scores, d3m.group_indices_val, group_losses)
+
+        sns.set_style("whitegrid")
+        fig, ax = plt.subplots(figsize=(12, 6))
+        ax.hist(d3m_scores, bins=50, alpha=0.7, color='blue', label='Training Samples')
+        ax.set_title(f'Distribution of Group Alignment Scores with {method}')
+        ax.legend()
+        fig.savefig(f'{args.path_to_save}attr_scores/Distribution_of_d3m_scores_{method}.pdf')
+
+        df_d3m_scores_stat.loc[method] = [d3m_scores.min(), d3m_scores.mean(), d3m_scores.max(), d3m_scores.std()]
 
  
-
     df_tda_scores_stat.to_csv(f'{args.path_to_save}attr_scores/tda_scores_stat.csv')
-    df_tda_scores_stat.to_csv(f'{args.path_to_save}attr_scores/tda_scores_group_stat.csv')
-
+    df_tda_scores_group_stat.to_csv(f'{args.path_to_save}attr_scores/tda_scores_group_stat.csv')
+    df_d3m_scores_stat.to_csv(f'{args.path_to_save}attr_scores/d3m_scores_stat.csv')
 
 
 
@@ -147,61 +163,6 @@ def main():
 
     df_metrics_group.to_csv(f'{args.path_to_save}attr_scores/metrics_group.csv')    
 
-
-
-
-
-    def get_dataloader_group(group):
-        g_indices = test_df.index[test_df["group"] == group].tolist()
-        g_test_dl = get_dataloader(test_dataset.select(g_indices), config.TEST_BATCH_SIZE)
-        return g_test_dl
-
-    test_dl_groups = {group:get_dataloader_group(group) for group in groups} 
-    
-    for k in ks:
-        print(k)
-
-        if args.method == "random":
-            debiased_train_idx = random.sample(range(len(train_dataset)), len(train_dataset)-k)
-        else:
-            debiased_train_idx = d3m.debias(num_to_discard=k)
-        
-
-        for i in range(args.num_runs):
-            new_folder = f"{args.output_dir}{args.method}_finetuning/{k}/{i}/"
-            os.makedirs(new_folder, exist_ok=True)
-    
-            seed = random.randint(0,1000)
-            finetune_model(train_dataset.select(debiased_train_idx),val_dataset, new_folder, random_seed=seed)
-        
-            model_path = f"{new_folder}/best_checkpoint"
-            model = AutoModelForSequenceClassification.from_pretrained(model_path,num_labels = 2).to(DEVICE)
-            model.eval()
-        
-            accuracy, loss, fpr, fnr, auc = compute_metrics(model, test_dl, DEVICE)
-            
-            dict_metrics[k]['accuracy'].append(accuracy)
-            dict_metrics[k]['loss'].append(loss)
-            dict_metrics[k]['fpr'].append(fpr)
-            dict_metrics[k]['fnr'].append(fnr)
-            dict_metrics[k]['auc'].append(auc)
-            
-            for group in groups:
-                accuracy,loss, fpr, fnr, auc = compute_metrics(model, test_dl_groups[group], DEVICE)
-                dict_metrics_groups[k][group]['accuracy'].append(accuracy)
-                dict_metrics_groups[k][group]['loss'].append(loss)
-                dict_metrics_groups[k][group]['fpr'].append(fpr)
-                dict_metrics_groups[k][group]['fnr'].append(fnr)
-                dict_metrics_groups[k][group]['auc'].append(auc)
-
-            shutil.rmtree(new_folder)
-
-
-        with open(f"{args.output_dir}{args.method}_finetuning/metrics_total_{k}.json", 'w') as f:
-            json.dump(dict_metrics, f)
-
-        with open(f"{args.output_dir}{args.method}_finetuning/metrics_groups_{k}.json", 'w') as f:
-            json.dump(dict_metrics_groups, f)
 
 if __name__ == "__main__":
     main()
