edb247ca958887500829484716558c236314dc7f
Anastasiia Alekseeva <alekseevaas21@gmail.com>
2024-09-30 00:41:07 +0200

    new heatmaps

--- exp_bert/vis_scores_label.py
+++ exp_bert/vis_scores_label.py
@@ -59,7 +59,7 @@ def main():
                 matrix_avg[i,j] = np.mean(m)
 
 
-        plt.figure(figsize=(6,2))
+        plt.figure(figsize=(2,6))
         
         # Create heatmap
         heatmap = plt.imshow(matrix_avg, cmap='viridis', aspect='auto')
